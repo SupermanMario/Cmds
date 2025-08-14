a253124b60565321ecebf14149539357e04bbb67
SupermanMario <144178885+SupermanMario@users.noreply.github.com>
2025-08-14 04:01:15 -0700

    Update Kick.lua

--- src/Kick.lua
+++ src/Kick.lua
@@ -1,10 +1,14 @@
 local Util = require(script.Parent.Parent.Util)
+local Notify = require(script.Parent.Parent.Notify)
 
 return {
     run = function(player, args)
         local target = Util:GetPlayerByName(args[2])
         if target then
             target:Kick("Kicked by " .. player.Name)
+            Notify:Send(player, "Kicked " .. target.Name)
+        else
+            Notify:Send(player, "Player not found")
         end
     end
 }
